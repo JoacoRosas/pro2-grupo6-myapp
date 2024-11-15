649bbc82322eb4a190ee7fbdc17ebf7ab8102564
Beniciogravier <bgravier@udesa.edu.ar>
2024-11-15 16:51:43 -0300

    merge

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,9 @@
-/*var express = require('express');
+var express = require('express');
 var router = express.Router();
 
 const indexController = require('../controllers/indexController')
 
 /* GET home page. */
-/*router.get('/', indexController.index);
-
-module.exports = router;*/
-
-
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.index);
 
 module.exports = router;
